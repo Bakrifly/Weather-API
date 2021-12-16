b372d9366d3bbe68594261492aed65a3d6610d4b
Bakrifly <bakri.hmouda@gmail.com>
2021-12-16 17:03:08 +0200

    Made the banner with search box

--- src/App.js
+++ src/App.js
@@ -1,32 +1,29 @@
-import React, { Component } from "react";
-import Search from "./components/Search";
+import React from "react";
 
-import SayHi, { SayHello } from "./components/WeatherItem";
-import fakeWeatherData from "./fakeWeatherData.json";
 
-import "./App.css";
-
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      name: "Bakri"
-    };
-  }
-
-  handleInputChange = value => {
-    this.setState({ name: value });
-  };
-
-  render() {
-    return (
-      <div className="app">
-        <SayHi />
-        <SayHello color="black" name={this.state.name} />
-        <Search handleInput={this.handleInputChange} />
-      </div>
-    );
-  }
-}
+const App = ()=>{
+ 
+  return (
+<div className="site-Wrapper" >
+<header>
+  <div className="Top-Banner">
+  <form className="search-wrapper">
+  <input type = "text" placeholder="Search Location..."></input>
+  <button>Search</button>
+  </form>
+  </div>
+  </header>
+  <div className="grid-container">
+    <div className="today-weather"></div>
+    <div className="hour3"></div>
+    <div className="hour6"></div>
+    <div className="hour9"></div>
+    <div className="hour12"></div>
+    <div className="hour15"></div>
+    <div className="hour18"></div>
+    <div className="hour21"></div>
+  </div>
+</div> )
+};
 
 export default App;
