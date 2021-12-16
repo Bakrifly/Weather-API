7e8affa71a37e4fea031da824a4f241d73df513b
Bakrifly <bakri.hmouda@gmail.com>
2021-12-16 20:43:13 +0200

    Adding placeHolder IMG

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import placeHolder from "./img/weather-icons/placeHolder.png"
 
 
 const App = ()=>{
@@ -13,16 +14,21 @@ const App = ()=>{
   </form>
   </div>
   </header>
+  <main>
   <div className="grid-container">
-    <div className="today-weather"></div>
-    <div className="hour3"></div>
-    <div className="hour6"></div>
-    <div className="hour9"></div>
-    <div className="hour12"></div>
-    <div className="hour15"></div>
-    <div className="hour18"></div>
-    <div className="hour21"></div>
+    <div className="today-weather"><img src={placeHolder}></img></div>
+    <div className="hour3"><p>03:00</p><img src={placeHolder}></img></div>
+    <div className="hour6"><p>06:00</p><img src={placeHolder}></img></div>
+    <div className="hour9"><p>09:00</p><img src={placeHolder}></img></div>
+    <div className="hour12"><p>12:00</p><img src={placeHolder}></img></div>
+    <div className="hour15"><p>15:00</p><img src={placeHolder}></img></div>
+    <div className="hour18"><p>18:00</p><img src={placeHolder}></img></div>
+    <div className="hour21"><p>21:00</p><img src={placeHolder}></img></div>
   </div>
+  </main>
+  <footer>
+    <div><p>Data Source: <a src = "https://openweathermap.org/">openweathermap.org</a> </p></div>
+  </footer>
 </div> )
 };
 
